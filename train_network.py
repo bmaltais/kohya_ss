6473aa1dd7cd927852957b977cfdf4f86b4f17b9
Isotr0py <2037008807@qq.com>
2023-02-08 21:32:21 +0800

    fix Input type error in noise_pred when using DDP

--- train_network.py
+++ train_network.py
@@ -417,7 +417,8 @@ def train(args):
 
         # Add noise to the latents according to the noise magnitude at each timestep
         # (this is the forward diffusion process)
-        noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
+        with autocast():
+          noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
 
         # Predict the noise residual
         noise_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
