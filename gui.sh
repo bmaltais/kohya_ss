c79181b7ea06732575b2f529387573eacdfcb8ea
jstayco <127801635+jstayco@users.noreply.github.com>
2023-03-27 18:58:30 -0700

    Update gui.sh
    Removed an unnecessary debug line.

--- gui.sh
+++ gui.sh
@@ -2,7 +2,7 @@
 
 # Activate the virtual environment
 source ./venv/bin/activate
-python -V
+
 # If the requirements are validated, run the kohya_gui.py script with the command-line arguments
 if python tools/validate_requirements.py; then
     python kohya_gui.py "$@"
