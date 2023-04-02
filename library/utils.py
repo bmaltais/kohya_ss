3f339cda6f760dae4aa4519600bdec79e765762b
ddPn08 <pyn.g.1125@gmail.com>
2023-04-02 23:21:17 +0900

    small fix

--- library/utils.py
+++ library/utils.py
@@ -2,7 +2,5 @@ import threading
 from typing import *
 
 
-def fire_in_thread(f):
-    def wrapped(*args, **kwargs):
-        threading.Thread(target=f, args=args, kwargs=kwargs).start()
-    return wrapped
+def fire_in_thread(f, *args, **kwargs):
+    threading.Thread(target=f, args=args, kwargs=kwargs).start()
