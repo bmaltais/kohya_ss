73a4d7407c1f3dffb795207afc4d018aed12e8f2
Jeremy Tayco <jstayco@protonmail.ch>
2023-04-05 00:23:58 -0700

    Removed old way to track custom arguments
    The new way is with a YAML file.

--- gui.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Activate the virtual environment
-source ./venv/bin/activate
-
-# If the requirements are validated, run the kohya_gui.py script with the command-line arguments
-if python tools/validate_requirements.py; then
-    python kohya_gui.py "$@"
-fi
--- setup.py
+++ setup.py
@@ -3,8 +3,4 @@ import subprocess
 import os
 import sys
 
-# Call the create_user_files.py script
-script_path = os.path.join("tools", "create_user_files.py")
-subprocess.run([sys.executable, script_path])
-
 setup(name="library", version="1.0.3", packages=find_packages())
