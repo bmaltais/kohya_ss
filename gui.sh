edd860f7eed4d1a95d75859c56f3bef55b0906ad
derVedro <derVedro@users.noreply.github.com>
2023-04-07 00:07:52 +0200

    Update gui.sh
    made gui start from any directory

--- gui.sh
+++ gui.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# Set root dir for the kohya-ss and change working directory
+KOHYA_SS_DIR=$(dirname "$(readlink -f -- "$0")")
+cd $KOHYA_SS_DIR
+
 # Activate the virtual environment
-source ./venv/bin/activate
+source "$KOHYA_SS_DIR"/venv/bin/activate
 
 # If the requirements are validated, run the kohya_gui.py script with the command-line arguments
-if python tools/validate_requirements.py; then
-    python kohya_gui.py "$@"
+if python "$KOHYA_SS_DIR"/tools/validate_requirements.py -r $KOHYA_SS_DIR/requirements.txt; then
+    python "$KOHYA_SS_DIR"/kohya_gui.py "$@"
 fi
+
+# step back
+cd -
