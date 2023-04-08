6a5f87d874031b66152de2356a96076061e596de
Kohya S <ykumeykume@gmail.com>
2023-04-08 21:45:57 +0900

    disable weighted captions in TI/XTI training

--- library/custom_train_functions.py
+++ library/custom_train_functions.py
@@ -18,19 +18,20 @@ def apply_snr_weight(loss, timesteps, noise_scheduler, gamma):
     return loss
 
 
-def add_custom_train_arguments(parser: argparse.ArgumentParser):
+def add_custom_train_arguments(parser: argparse.ArgumentParser, support_weighted_captions: bool = True):
     parser.add_argument(
         "--min_snr_gamma",
         type=float,
         default=None,
         help="gamma for reducing the weight of high loss timesteps. Lower numbers have stronger effect. 5 is recommended by paper. / 低いタイムステップでの高いlossに対して重みを減らすためのgamma値、低いほど効果が強く、論文では5が推奨",
     )
-    parser.add_argument(
-        "--weighted_captions",
-        action="store_true",
-        default=False,
-        help="Enable weighted captions in the standard style (token:1.3). No commas inside parens, or shuffle/dropout may break the decoder.",
-    )
+    if support_weighted_captions:
+        parser.add_argument(
+            "--weighted_captions",
+            action="store_true",
+            default=False,
+            help="Enable weighted captions in the standard style (token:1.3). No commas inside parens, or shuffle/dropout may break the decoder. / 「[token]」、「(token)」「(token:1.3)」のような重み付きキャプションを有効にする。カンマを括弧内に入れるとシャッフルやdropoutで重みづけがおかしくなるので注意",
+        )
 
 
 re_attention = re.compile(
