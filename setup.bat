f1934f264a867ec74457f968f0d37c4272160c63
bmaltais <bernard@ducourier.com>
2023-04-15 19:46:57 -0400

    Improving uninstall of modules

--- setup.bat
+++ setup.bat
@@ -21,8 +21,8 @@ echo [2] - No
 set /p uninstall_choice="Enter your choice (1 or 2): "
 
 if %uninstall_choice%==1 (
-    pip uninstall xformers
-    pip uninstall torch torchvision
+    pip uninstall -y xformers
+    pip uninstall -y torch torchvision
 )
 
 echo Please choose the version of torch you want to install:
@@ -37,7 +37,8 @@ if %choice%==1 (
 ) else (
     pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
     pip install --use-pep517 --upgrade -r requirements.txt
-    pip install -U -I --no-deps https://files.pythonhosted.org/packages/d6/f7/02662286419a2652c899e2b3d1913c47723fc164b4ac06a85f769c291013/xformers-0.0.17rc482-cp310-cp310-win_amd64.whl
+    pip install --upgrade  xformers==0.0.17
+    rem pip install -U -I --no-deps https://files.pythonhosted.org/packages/d6/f7/02662286419a2652c899e2b3d1913c47723fc164b4ac06a85f769c291013/xformers-0.0.17rc482-cp310-cp310-win_amd64.whl
 )
 
 copy /y .\bitsandbytes_windows\*.dll .\venv\Lib\site-packages\bitsandbytes\
