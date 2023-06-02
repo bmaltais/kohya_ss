bdca1464d5b61a1e71c586b4e5f346cd38991cd4
bmaltais <bernard@ducourier.com>
2023-06-02 10:30:52 -0400

    Update log outputs for requirements and headless

--- tools/validate_requirements.py
+++ tools/validate_requirements.py
@@ -1,37 +1,17 @@
 import os
 import sys
-import pkg_resources
-import argparse
 import shutil
-import logging
 import time
+import re
+import argparse
+import pkg_resources
+from packaging.requirements import Requirement
+from packaging.markers import default_environment
 
-log = logging.getLogger("sd")
+from library.custom_logging import setup_logging
 
-# setup console and file logging
-def setup_logging(clean=False):
-    try:
-        if clean and os.path.isfile('setup.log'):
-            os.remove('setup.log')
-        time.sleep(0.1) # prevent race condition
-    except:
-        pass
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(pathname)s | %(message)s', filename='setup.log', filemode='a', encoding='utf-8', force=True)
-    from rich.theme import Theme
-    from rich.logging import RichHandler
-    from rich.console import Console
-    from rich.pretty import install as pretty_install
-    from rich.traceback import install as traceback_install
-    console = Console(log_time=True, log_time_format='%H:%M:%S-%f', theme=Theme({
-        "traceback.border": "black",
-        "traceback.border.syntax_error": "black",
-        "inspect.value.border": "black",
-    }))
-    pretty_install(console=console)
-    traceback_install(console=console, extra_lines=1, width=console.width, word_wrap=False, indent_guides=False, suppress=[])
-    rh = RichHandler(show_time=True, omit_repeated_times=False, show_level=True, show_path=False, markup=False, rich_tracebacks=True, log_time_format='%H:%M:%S-%f', level=logging.DEBUG if args.debug else logging.INFO, console=console)
-    rh.set_name(logging.DEBUG if args.debug else logging.INFO)
-    log.addHandler(rh)
+# Set up logging
+log = setup_logging()
 
 def check_torch():
     if shutil.which('nvidia-smi') is not None or os.path.exists(os.path.join(os.environ.get('SystemRoot') or r'C:\Windows', 'System32', 'nvidia-smi.exe')):
@@ -40,11 +20,13 @@ def check_torch():
         log.info('AMD toolkit detected')
     else:
         log.info('Using CPU-only Torch')
+
     try:
         import torch
         log.info(f'Torch {torch.__version__}')
+        
         if not torch.cuda.is_available():
-            log.warning("Torch repoorts CUDA not available")
+            log.warning("Torch reports CUDA not available")
         else:
             if torch.version.cuda:
                 log.info(f'Torch backend: nVidia CUDA {torch.version.cuda} cuDNN {torch.backends.cudnn.version() if torch.backends.cudnn.is_available() else "N/A"}')
@@ -52,84 +34,99 @@ def check_torch():
                 log.info(f'Torch backend: AMD ROCm HIP {torch.version.hip}')
             else:
                 log.warning('Unknown Torch backend')
+            
             for device in [torch.cuda.device(i) for i in range(torch.cuda.device_count())]:
                 log.info(f'Torch detected GPU: {torch.cuda.get_device_name(device)} VRAM {round(torch.cuda.get_device_properties(device).total_memory / 1024 / 1024)} Arch {torch.cuda.get_device_capability(device)} Cores {torch.cuda.get_device_properties(device).multi_processor_count}')
     except Exception as e:
         log.error(f'Could not load torch: {e}')
-        exit(1)
-from packaging.requirements import Requirement
-from packaging.markers import default_environment
-import re
+        sys.exit(1)
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description="Validate that requirements are satisfied.")
-parser.add_argument('-r', '--requirements', type=str, default='requirements.txt', help="Path to the requirements file.")
-parser.add_argument(
-        '--debug', action='store_true', help='Debug on'
-    )
-args = parser.parse_args()
 
-setup_logging()
-check_torch()
-
-log.info("Validating that requirements are satisfied.")
-
-# Load the requirements from the specified requirements file
-with open(args.requirements) as f:
-    requirements = f.readlines()
-
-# Check each requirement against the installed packages
-missing_requirements = []
-wrong_version_requirements = []
-
-url_requirement_pattern = re.compile(r"(?P<url>https?://.+);?\s?(?P<marker>.+)?")
-
-for requirement in requirements:
-    requirement = requirement.strip()
-    if requirement == ".":
-        # Skip the current requirement if it is a dot (.)
-        continue
+def validate_requirements(requirements_file):
+    log.info("Validating that requirements are satisfied.")
     
-    url_match = url_requirement_pattern.match(requirement)
-    if url_match:
-        if url_match.group("marker"):
-            marker = url_match.group("marker")
-            parsed_marker = Marker(marker)
-            if not parsed_marker.evaluate(default_environment()):
-                continue
-        requirement = url_match.group("url")
+    with open(requirements_file) as f:
+        requirements = f.readlines()
 
-    try:
-        parsed_req = Requirement(requirement)
+    missing_requirements = []
+    wrong_version_requirements = []
+    url_requirement_pattern = re.compile(r"(?P<url>https?://.+);?\s?(?P<marker>.+)?")
+
+    for requirement in requirements:
+        requirement = requirement.strip()
         
-        # Check if the requirement has an environment marker and if it evaluates to False
-        if parsed_req.marker and not parsed_req.marker.evaluate(default_environment()):
+        if requirement == ".":
+            # Skip the current requirement if it is a dot (.)
             continue
         
-        pkg_resources.require(str(parsed_req))
-    except ValueError:
-        # This block will handle URL-based requirements
-        pass
-    except pkg_resources.DistributionNotFound:
-        missing_requirements.append(requirement)
-    except pkg_resources.VersionConflict as e:
-        wrong_version_requirements.append((requirement, str(e.req), e.dist.version))
+        url_match = url_requirement_pattern.match(requirement)
+        
+        if url_match:
+            if url_match.group("marker"):
+                marker = url_match.group("marker")
+                parsed_marker = Marker(marker)
+                
+                if not parsed_marker.evaluate(default_environment()):
+                    continue
+            
+            requirement = url_match.group("url")
 
-# If there are any missing or wrong version requirements, print an error message and exit with a non-zero exit code
-if missing_requirements or wrong_version_requirements:
-    if missing_requirements:
-        log.info("Error: The following packages are missing:")
-        for requirement in missing_requirements:
-            log.info(f" - {requirement}")
-    if wrong_version_requirements:
-        log.info("Error: The following packages have the wrong version:")
-        for requirement, expected_version, actual_version in wrong_version_requirements:
-            log.info(f" - {requirement} (expected version {expected_version}, found version {actual_version})")
-    upgrade_script = "upgrade.ps1" if os.name == "nt" else "upgrade.sh"
-    log.info(f"\nRun \033[33m{upgrade_script}\033[0m or \033[33mpip install -U -r {args.requirements}\033[0m to resolve the missing requirements listed above...")
+        try:
+            parsed_req = Requirement(requirement)
+            
+            if parsed_req.marker and not parsed_req.marker.evaluate(default_environment()):
+                continue
+            
+            pkg_resources.require(str(parsed_req))
+        except ValueError:
+            # This block will handle URL-based requirements
+            pass
+        except pkg_resources.DistributionNotFound:
+            missing_requirements.append(requirement)
+        except pkg_resources.VersionConflict as e:
+            wrong_version_requirements.append((requirement, str(e.req), e.dist.version))
 
-    sys.exit(1)
+    return missing_requirements, wrong_version_requirements
 
-# All requirements satisfied
-log.info("All requirements satisfied.")
-sys.exit(0)
+
+def print_error_messages(missing_requirements, wrong_version_requirements, requirements_file):
+    if missing_requirements or wrong_version_requirements:
+        if missing_requirements:
+            log.info("Error: The following packages are missing:")
+            
+            for requirement in missing_requirements:
+                log.info(f" - {requirement}")
+        
+        if wrong_version_requirements:
+            log.info("Error: The following packages have the wrong version:")
+            
+            for requirement, expected_version, actual_version in wrong_version_requirements:
+                log.info(f" - {requirement} (expected version {expected_version}, found version {actual_version})")
+        
+        upgrade_script = "upgrade.ps1" if os.name == "nt" else "upgrade.sh"
+        log.info(f"\nRun \033[33m{upgrade_script}\033[0m or \033[33mpip install -U -r {requirements_file}\033[0m to resolve the missing requirements listed above...")
+        sys.exit(1)
+
+    log.info("All requirements satisfied.")
+    sys.exit(0)
+
+
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description="Validate that requirements are satisfied.")
+    parser.add_argument('-r', '--requirements', type=str, default='requirements.txt', help="Path to the requirements file.")
+    parser.add_argument('--debug', action='store_true', help='Debug on')
+    args = parser.parse_args()
+
+    # Check Torch
+    check_torch()
+
+    # Validate requirements
+    missing_requirements, wrong_version_requirements = validate_requirements(args.requirements)
+
+    # Print error messages if there are missing or wrong version requirements
+    print_error_messages(missing_requirements, wrong_version_requirements, args.requirements)
+
+
+if __name__ == "__main__":
+    main()
