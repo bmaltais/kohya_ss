e387dda2593eef6be0d835c4ba8757d8a0a7dfdb
bmaltais <bernard@ducourier.com>
2023-06-04 17:09:00 -0400

    New setup.bat version using python under the hood

--- setup.bat
+++ setup.bat
@@ -24,30 +24,4 @@ python .\tools\check_local_modules.py
 
 call .\venv\Scripts\activate.bat
 
-:: Upgrade pip if needed
-python.exe -m pip install --upgrade pip
-
-echo.
-echo Please choose the version of torch you want to install:
-echo [1] - v1 (torch 1.12.1) (Recommended for best compatibility)
-echo [2] - v2 (torch 2.0.0) (Experimental, faster but more prone to issues)
-set /p choice="Enter your choice (1 or 2): "
-
-if %choice%==1 (
-    pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
-    del ".\logs\status\torch_version" > nul 2>&1
-    echo 1 > ".\logs\status\torch_version"
-    pip install --use-pep517 --upgrade -r requirements_windows_torch1.txt
-    pip install -U -I --no-deps https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/f/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
-) else (
-    pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-    del ".\logs\status\torch_version" > nul 2>&1
-    echo 2 > ".\logs\status\torch_version"
-    pip install --use-pep517 --upgrade -r requirements_windows_torch2.txt
-    pip install --upgrade xformers==0.0.20
-    pip install https://huggingface.co/r4ziel/xformers_pre_built/resolve/main/triton-2.0.0-cp310-cp310-win_amd64.whl
-)
-
-python.exe .\tools\update_bitsandbytes.py
-
-accelerate config
+python .\tools\setup_windows.py
--- tools/validate_requirements.py
+++ tools/validate_requirements.py
@@ -1,19 +1,15 @@
 import os
 import sys
 import shutil
-import time
-import re
 import argparse
-import pkg_resources
-from packaging.requirements import Requirement
-from packaging.markers import default_environment
-
+from setup_windows import install
 from library.custom_logging import setup_logging
 
 # Set up logging
 log = setup_logging()
 
 def check_torch():
+    # Check for nVidia toolkit or AMD toolkit
     if shutil.which('nvidia-smi') is not None or os.path.exists(os.path.join(os.environ.get('SystemRoot') or r'C:\Windows', 'System32', 'nvidia-smi.exe')):
         log.info('nVidia toolkit detected')
     elif shutil.which('rocminfo') is not None or os.path.exists('/opt/rocm/bin/rocminfo'):
@@ -25,16 +21,20 @@ def check_torch():
         import torch
         log.info(f'Torch {torch.__version__}')
         
+        # Check if CUDA is available
         if not torch.cuda.is_available():
             log.warning("Torch reports CUDA not available")
         else:
             if torch.version.cuda:
+                # Log nVidia CUDA and cuDNN versions
                 log.info(f'Torch backend: nVidia CUDA {torch.version.cuda} cuDNN {torch.backends.cudnn.version() if torch.backends.cudnn.is_available() else "N/A"}')
             elif torch.version.hip:
+                # Log AMD ROCm HIP version
                 log.info(f'Torch backend: AMD ROCm HIP {torch.version.hip}')
             else:
                 log.warning('Unknown Torch backend')
             
+            # Log information about detected GPUs
             for device in [torch.cuda.device(i) for i in range(torch.cuda.device_count())]:
                 log.info(f'Torch detected GPU: {torch.cuda.get_device_name(device)} VRAM {round(torch.cuda.get_device_properties(device).total_memory / 1024 / 1024)} Arch {torch.cuda.get_device_capability(device)} Cores {torch.cuda.get_device_properties(device).multi_processor_count}')
     except Exception as e:
@@ -42,74 +42,15 @@ def check_torch():
         sys.exit(1)
 
 
-def validate_requirements(requirements_file):
-    log.info("Validating that requirements are satisfied.")
-    
-    with open(requirements_file) as f:
-        requirements = f.readlines()
-
-    missing_requirements = []
-    wrong_version_requirements = []
-    url_requirement_pattern = re.compile(r"(?P<url>https?://.+);?\s?(?P<marker>.+)?")
-
-    for requirement in requirements:
-        requirement = requirement.strip()
+def install_requirements(requirements_file):
+    log.info('Verifying requirements')
+    with open(requirements_file, 'r', encoding='utf8') as f:
+        # Read lines from the requirements file, strip whitespace, and filter out empty lines, comments, and lines starting with '.'
+        lines = [line.strip() for line in f.readlines() if line.strip() != '' and not line.startswith('#') and line is not None and not line.startswith('.')]
         
-        if requirement == ".":
-            # Skip the current requirement if it is a dot (.)
-            continue
-        
-        url_match = url_requirement_pattern.match(requirement)
-        
-        if url_match:
-            if url_match.group("marker"):
-                marker = url_match.group("marker")
-                parsed_marker = Marker(marker)
-                
-                if not parsed_marker.evaluate(default_environment()):
-                    continue
-            
-            requirement = url_match.group("url")
-
-        try:
-            parsed_req = Requirement(requirement)
-            
-            if parsed_req.marker and not parsed_req.marker.evaluate(default_environment()):
-                continue
-            
-            pkg_resources.require(str(parsed_req))
-        except ValueError:
-            # This block will handle URL-based requirements
-            pass
-        except pkg_resources.DistributionNotFound:
-            missing_requirements.append(requirement)
-        except pkg_resources.VersionConflict as e:
-            wrong_version_requirements.append((requirement, str(e.req), e.dist.version))
-
-    return missing_requirements, wrong_version_requirements
-
-
-def print_error_messages(missing_requirements, wrong_version_requirements, requirements_file):
-    if missing_requirements or wrong_version_requirements:
-        if missing_requirements:
-            log.info("Error: The following packages are missing:")
-            
-            for requirement in missing_requirements:
-                log.info(f" - {requirement}")
-        
-        if wrong_version_requirements:
-            log.info("Error: The following packages have the wrong version:")
-            
-            for requirement, expected_version, actual_version in wrong_version_requirements:
-                log.info(f" - {requirement} (expected version {expected_version}, found version {actual_version})")
-        
-        upgrade_script = "upgrade.bat" if os.name == "nt" else "upgrade.sh"
-        log.info(f"\nRun {upgrade_script} to resolve the missing requirements listed above...")
-        sys.exit(1)
-
-    log.info("All requirements satisfied.")
-    sys.exit(0)
-
+        # Iterate over each line and install the requirements
+        for line in lines:
+            install(line)
 
 def main():
     # Parse command line arguments
@@ -120,12 +61,7 @@ def main():
 
     # Check Torch
     check_torch()
-
-    # Validate requirements
-    missing_requirements, wrong_version_requirements = validate_requirements(args.requirements)
-
-    # Print error messages if there are missing or wrong version requirements
-    print_error_messages(missing_requirements, wrong_version_requirements, args.requirements)
+    install_requirements(args.requirements)
 
 
 if __name__ == "__main__":
