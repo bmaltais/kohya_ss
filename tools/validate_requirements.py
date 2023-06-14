e59802f0526d89fc9ab1888c6883c818828ce427
primeinc <4395149+primeinc@users.noreply.github.com>
2023-06-14 14:43:29 -0400

    Fixed importing custom_logging

--- tools/validate_requirements.py
+++ tools/validate_requirements.py
@@ -3,6 +3,14 @@ import sys
 import shutil
 import argparse
 from setup_windows import install
+
+# Get the absolute path of the Kohua_SS project directory
+project_directory = os.path.dirname(os.path.abspath(__file__))
+if "tools" in project_directory:
+    project_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Add the project directory to the Python search path
+sys.path.insert(0, project_directory)
+
 from library.custom_logging import setup_logging
 
 # Set up logging
