20c3f0c61be7d5a944d9b04dff0c00084623fee3
bmaltais <bernard@ducourier.com>
2023-06-23 07:26:52 -0400

    Making linux setup use more python

---
+++ setup/setup_linux.py
@@ -0,0 +1,39 @@
+import argparse
+import logging
+import setup_common
+
+errors = 0  # Define the 'errors' variable before using it
+log = logging.getLogger('sd')
+
+# ANSI escape code for yellow color
+YELLOW = '\033[93m'
+RESET_COLOR = '\033[0m'
+
+
+def install_kohya_ss(platform_requirements_file):
+    setup_common.check_repo_version()
+    setup_common.check_python()
+
+    # Upgrade pip if needed
+    setup_common.install('--upgrade pip')
+
+    setup_common.install_requirements(platform_requirements_file)
+    
+    setup_common.install_requirements('requirements.txt')
+    # setup_common.configure_accelerate()
+    # run_cmd(f'accelerate config')
+
+
+def main_menu(platform_requirements_file):
+    install_kohya_ss(platform_requirements_file)
+
+
+if __name__ == '__main__':
+    setup_common.ensure_base_requirements()
+    setup_common.setup_logging()
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--platform-requirements-file', dest='platform_requirements_file', default='requirements_linux.txt', help='Path to the platform-specific requirements file')
+    args = parser.parse_args()
+    
+    main_menu(args.platform_requirements_file)
