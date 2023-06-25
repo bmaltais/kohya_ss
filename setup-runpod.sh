629819e40849017d31eadcc74f9f0e81f8c76bab
bmaltais <bernard@ducourier.com>
2023-06-24 20:52:00 -0400

    Update runpod

--- setup-runpod.sh
+++ setup-runpod.sh
@@ -3,15 +3,15 @@
 # This gets the directory the script is run from so pathing can work relative to the script where needed.
 SCRIPT_DIR="$(cd -- $(dirname -- "$0") && pwd)"
 
+# Install tk
+echo "Install tk and python3.10-venv..."
+apt update -y && apt install -y python3-tk python3.10-venv
+
 # Check if the venv folder doesn't exist
 if [ ! -d "$SCRIPT_DIR/venv" ]; then
     echo "Creating venv..."
     python3 -m venv "$SCRIPT_DIR/venv"
 fi
-
-# Install tk
-echo "Install tk..."
-apt update -y && apt install -y python3-tk python3.10-venv
 
 # Activate the virtual environment
 echo "Activate venv..."
