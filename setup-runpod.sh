9be16517d800974b5321afa568e5eec1f4ff2168
bmaltais <bernard@ducourier.com>
2023-06-25 11:03:32 -0400

    Update runpod setup

--- setup-runpod.sh
+++ setup-runpod.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # This gets the directory the script is run from so pathing can work relative to the script where needed.
-SCRIPT_DIR="$(cd -- $(dirname -- "$0") && pwd)"
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
 
-# Install tk
-echo "Install tk and python3.10-venv..."
+# Install tk and python3.10-venv
+echo "Installing tk and python3.10-venv..."
 apt update -y && apt install -y python3-tk python3.10-venv
 
 # Check if the venv folder doesn't exist
@@ -14,12 +14,14 @@ if [ ! -d "$SCRIPT_DIR/venv" ]; then
 fi
 
 # Activate the virtual environment
-echo "Activate venv..."
+echo "Activating venv..."
 source "$SCRIPT_DIR/venv/bin/activate" || exit 1
 
 # Run setup_linux.py script with platform requirements
+echo "Running setup_linux.py..."
 python "$SCRIPT_DIR/setup/setup_linux.py" --platform-requirements-file=requirements_runpod.txt
 
 # Configure accelerate
-echo "Accelerate config..."
-accelerate config
+echo "Configuring accelerate..."
+cp "$SCRIPT_DIR/config_files/accelerate/runpod.yaml" "/root/.cache/huggingface/accelerate/default_config.yaml"
+echo "To manually configure accelerate run: $SCRIPT_DIR/venv/bin/activate"
