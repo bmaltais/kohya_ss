d46ce82576b3f43d5af19afc9fa880e2773ea67c
Steven.chen <stevenchenworking@gmail.com>
2023-06-29 15:14:17 +0800

    [steven] add centralized config file

---
+++ library/config.py
@@ -0,0 +1,3 @@
+# Tokenizer: checkpointから読み込むのではなくあらかじめ提供されているものを使う
+TOKENIZER_PATH = "openai/clip-vit-large-patch14"
+V2_STABLE_DIFFUSION_PATH = "stabilityai/stable-diffusion-2"  # ここからtokenizerだけ使う v2とv2.1はtokenizer仕様は同じ
