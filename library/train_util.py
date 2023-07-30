9ec70252d0d687522544841a23744c8708c39d15
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-07-30 12:15:13 +0800

    Add Paged/ adam8bit/lion8bit for Sdxl bitsandbytes 0.39.1 cuda118 on windows (#623)
    * ADD libbitsandbytes.dll for 0.38.1
    
    * Delete libbitsandbytes_cuda116.dll
    
    * Delete cextension.py
    
    * add main.py
    
    * Update requirements.txt for bitsandbytes 0.38.1
    
    * Update README.md for bitsandbytes-windows
    
    * Update README-ja.md  for bitsandbytes 0.38.1
    
    * Update main.py for return cuda118
    
    * Update train_util.py for lion8bit
    
    * Update train_README-ja.md for lion8bit
    
    * Update train_util.py for add DAdaptAdan and DAdaptSGD
    
    * Update train_util.py for DAdaptadam
    
    * Update train_network.py for dadapt
    
    * Update train_README-ja.md for DAdapt
    
    * Update train_util.py for DAdapt
    
    * Update train_network.py for DAdaptAdaGrad
    
    * Update train_db.py for DAdapt
    
    * Update fine_tune.py for DAdapt
    
    * Update train_textual_inversion.py for DAdapt
    
    * Update train_textual_inversion_XTI.py for DAdapt
    
    * Revert "Merge branch 'qinglong' into main"
    
    This reverts commit b65c023083d6d1e8a30eb42eddd603d1aac97650, reversing
    changes made to f6fda20caf5e773d56bcfb5c4575c650bb85362b.
    
    * Revert "Update requirements.txt for bitsandbytes 0.38.1"
    
    This reverts commit 83abc60dfaddb26845f54228425b98dd67997528.
    
    * Revert "Delete cextension.py"
    
    This reverts commit 3ba4dfe046874393f2a022a4cbef3628ada35391.
    
    * Revert "Update README.md for bitsandbytes-windows"
    
    This reverts commit 4642c52086b5e9791233007e2fdfd97f832cd897.
    
    * Revert "Update README-ja.md  for bitsandbytes 0.38.1"
    
    This reverts commit fa6d7485ac067ebc49e6f381afdb8dd2f12caa8f.
    
    * Update train_util.py for DAdaptLion
    
    * Update train_README-zh.md for dadaptlion
    
    * Update train_README-ja.md for DAdaptLion
    
    * add DAdatpt V3
    
    * Alignment
    
    * Update train_util.py for experimental
    
    * Update train_util.py V3
    
    * Update train_util.py
    
    * Update requirements.txt
    
    * Update train_README-zh.md
    
    * Update train_README-ja.md
    
    * Update train_util.py fix
    
    * Update train_util.py
    
    * support Prodigy
    
    * add lower
    
    * Update main.py
    
    * support PagedAdamW8bit/PagedLion8bit
    
    * Update requirements.txt
    
    * update for PageAdamW8bit and PagedLion8bit
    
    * Revert
    
    * revert main
    
    * Update train_util.py
    
    * update for bitsandbytes 0.39.1
    
    * Update requirements.txt
    
    * vram leak fix
    
    ---------
    
    Co-authored-by: Pam <pamhome21@gmail.com>

--- library/train_util.py
+++ library/train_util.py
@@ -2165,6 +2165,8 @@ def cache_batch_latents(
             info.latents = latent
             if flip_aug:
                 info.latents_flipped = flipped_latent
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
 
 
 def cache_batch_text_encoder_outputs(
