978b93a9a8e99afd011acc740fa958eda301f41d
MizunagiKB <mizukb@LIVE.JP>
2023-08-14 13:23:43 +0900

    append mzworks scripts

---
+++ mzworks_tag.py
@@ -0,0 +1,56 @@
+import argparse
+import pathlib
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dir", default="./dataset/train")
+    parser.add_argument("--update", default="")
+    parser.add_argument("--append", default="")
+    parser.add_argument("--remove", default="")
+    parser.add_argument("--dry-run", action="store_true")
+
+    args = parser.parse_args()
+
+    o_path = pathlib.Path(args.train_dir)
+
+    dict_caption = {}
+    dict_tag = {}
+
+    for o_pathname in o_path.glob("*.caption"):
+        pathname = str(o_pathname)
+
+        with open(pathname, "r") as rf:
+            if args.update != "":
+                list_tag = [v.strip() for v in args.update.split(",")]
+            else:
+                list_tag = [v.strip() for v in rf.read().split(",")]
+
+            dict_caption[pathname] = list_tag
+            for tag in list_tag:
+                try:
+                    dict_tag[tag] += 1
+                except KeyError:
+                    dict_tag[tag] = 1
+
+    if args.append != "":
+        for pathname, list_tag in dict_caption.items():
+            if args.append in list_tag:
+                list_tag.remove(args.append)
+
+            list_tag.insert(0, args.append)
+
+    if args.remove != "":
+        for pathname, list_tag in dict_caption.items():
+            if args.remove in list_tag:
+                list_tag.remove(args.remove)
+
+    if args.dry_run is False:
+        for pathname, list_tag in dict_caption.items():
+            with open(pathname, "w") as wf:
+                wf.write(", ".join(list_tag))
+
+
+if __name__ == "__main__":
+    main()
