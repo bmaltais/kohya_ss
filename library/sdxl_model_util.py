2e0942d5c80b2903cf5eb6a0ba90fd0e64e80789
Isotr0py <2037008807@qq.com>
2023-08-27 20:45:40 +0800

    delet missed line

--- library/sdxl_model_util.py
+++ library/sdxl_model_util.py
@@ -259,7 +259,6 @@ def load_models_from_sdxl_checkpoint(model_version, ckpt_path, map_location, dty
         elif k.startswith("conditioner.embedders.1.model."):
             te2_sd[k] = state_dict.pop(k)
 
-    info1 = text_model1.load_state_dict(te1_sd)
     info1 = _load_state_dict_on_device(text_model1, te1_sd, device=map_location)    # remain fp32
     print("text encoder 1:", info1)
 
