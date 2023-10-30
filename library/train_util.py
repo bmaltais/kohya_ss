583e2b2d0174e9a8bebcabfa178295e2980d334c
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-10-30 20:02:04 +0800

    Fix typo

--- library/train_util.py
+++ library/train_util.py
@@ -341,7 +341,7 @@ class BaseSubset:
         image_dir: Optional[str],
         num_repeats: int,
         shuffle_caption: bool,
-        caption_seperator: str,
+        caption_separator: str,
         keep_tokens: int,
         color_aug: bool,
         flip_aug: bool,
@@ -358,7 +358,7 @@ class BaseSubset:
         self.image_dir = image_dir
         self.num_repeats = num_repeats
         self.shuffle_caption = shuffle_caption
-        self.caption_seperator = caption_seperator
+        self.caption_separator = caption_separator
         self.keep_tokens = keep_tokens
         self.color_aug = color_aug
         self.flip_aug = flip_aug
@@ -385,7 +385,7 @@ class DreamBoothSubset(BaseSubset):
         caption_extension: str,
         num_repeats,
         shuffle_caption,
-        caption_seperator: str,
+        caption_separator: str,
         keep_tokens,
         color_aug,
         flip_aug,
@@ -405,7 +405,7 @@ class DreamBoothSubset(BaseSubset):
             image_dir,
             num_repeats,
             shuffle_caption,
-            caption_seperator,
+            caption_separator,
             keep_tokens,
             color_aug,
             flip_aug,
@@ -439,7 +439,7 @@ class FineTuningSubset(BaseSubset):
         metadata_file: str,
         num_repeats,
         shuffle_caption,
-        caption_seperator,
+        caption_separator,
         keep_tokens,
         color_aug,
         flip_aug,
@@ -459,7 +459,7 @@ class FineTuningSubset(BaseSubset):
             image_dir,
             num_repeats,
             shuffle_caption,
-            caption_seperator,
+            caption_separator,
             keep_tokens,
             color_aug,
             flip_aug,
@@ -490,7 +490,7 @@ class ControlNetSubset(BaseSubset):
         caption_extension: str,
         num_repeats,
         shuffle_caption,
-        caption_seperator,
+        caption_separator,
         keep_tokens,
         color_aug,
         flip_aug,
@@ -510,7 +510,7 @@ class ControlNetSubset(BaseSubset):
             image_dir,
             num_repeats,
             shuffle_caption,
-            caption_seperator,
+            caption_separator,
             keep_tokens,
             color_aug,
             flip_aug,
@@ -646,7 +646,7 @@ class BaseDataset(torch.utils.data.Dataset):
             caption = ""
         else:
             if subset.shuffle_caption or subset.token_warmup_step > 0 or subset.caption_tag_dropout_rate > 0:
-                tokens = [t.strip() for t in caption.strip().split(subset.caption_seperator)]
+                tokens = [t.strip() for t in caption.strip().split(subset.caption_separator)]
                 if subset.token_warmup_step < 1:  # 初回に上書きする
                     subset.token_warmup_step = math.floor(subset.token_warmup_step * self.max_train_steps)
                 if subset.token_warmup_step and self.current_step < subset.token_warmup_step:
@@ -3102,7 +3102,7 @@ def add_dataset_arguments(
         "--shuffle_caption", action="store_true", help="shuffle separated caption / 区切られたcaptionの各要素をshuffleする"
     )
     parser.add_argument(
-        "--caption_seperator", type=str, default=",", help="seperator for caption / captionの区切り文字"
+        "--caption_separator", type=str, default=",", help="separator for caption / captionの区切り文字"
     )
     parser.add_argument(
         "--caption_extension", type=str, default=".caption", help="extension of caption files / 読み込むcaptionファイルの拡張子"
