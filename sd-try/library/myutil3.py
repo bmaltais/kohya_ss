152377cb07575c0680c0bbe39cff155b63abde27
UnkownWorld <49099528+UnkownWorld@users.noreply.github.com>
2024-05-24 09:03:28 +0800

    Update myutil3.py

--- sd-try/library/myutil3.py
+++ sd-try/library/myutil3.py
@@ -81,8 +81,8 @@ class AdaptiveLoss(nn.Module):
         print(f"myutil——output:{output.shape},max:{torch.max(output)},min:{torch.min(output)}")
         huber_loss = self.huber_loss(output, target)
         l2_loss = self.l2_loss(output, target)
-        print(f"myutil—— huber_loss:{huber_loss.shape},max:{torch.max(huber_loss)},min:{torch.min(huber_loss)},{huber_loss}")
-        print(f"myutil—— l2_loss:{l2_loss.shape},max:{torch.max(l2_loss)},min:{torch.min(l2_loss)},{l2_loss}")
+        print(f"myutil—— huber_loss:{huber_loss.shape},max:{torch.max(huber_loss)},min:{torch.min(huber_loss)}")
+        print(f"myutil—— l2_loss:{l2_loss.shape},max:{torch.max(l2_loss)},min:{torch.min(l2_loss)}")
         combined_loss = torch.stack([huber_loss, l2_loss], dim=-1)  # 形状为[2,4,64,64,2]
         print(f"myutil—— combined_loss1:{combined_loss.shape},max:{torch.max(combined_loss)},min:{torch.min(combined_loss)}")
         batch_size, channels, height, width, _ = combined_loss.shape
