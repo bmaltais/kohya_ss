a305c38d8341452708253e08750489d1fc1c878a
UnkownWorld <49099528+UnkownWorld@users.noreply.github.com>
2024-05-27 09:22:36 +0800

    Update latent_util2.py

--- sd-try/latent_util2.py
+++ sd-try/latent_util2.py
@@ -2,16 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-class MyModel(nn.Module):
-    def __init__(self,device):
-        super(MyModel, self).__init__()
-        #self.conv1 = nn.Conv2d(4, 8, kernel_size=3, padding=1).to(device)
-        self.lp_pool = nn.LPPool2d(2, kernel_size=2, stride=2, ceil_mode=False).to(device)
-        
-    def forward(self, x):
-        #x = self.conv1(x)
-        x = self.lp_pool(x)
-        return x
+
 
 class Upsampling(nn.Module):
     def __init__(self):
@@ -29,23 +20,17 @@ class ProcessLatents(nn.Module):
         self.lp_pool = nn.LPPool2d(2, kernel_size=2, stride=2, ceil_mode=False).to(device)
         self.upsampling = Upsampling()
 
-    def forward(self, x, device, is_for_height=False):
-        noisy_latents = self.make_latents(x, -1, device)
+    def forward(self, x, device, dim):
+        noisy_latents = self.make_latents(x, dim, device)
         if is_for_height:
             noisy_latents2 = self.make_latents(x, -2, device)
-            noisy_latents = torch.cat([noisy_latents, noisy_latents2], dim=-4)
-            #noisy_latents = self.conv1(noisy_latents)
-        else:
-            noisy_latents = self.conv2(noisy_latents)
+            noisy_latents = (noisy_latents + noisy_latents2) / 2
         return noisy_latents
 
     def make_latents(self, noisy_latents, dim, device):
         split_size = noisy_latents.size(dim) // 2
         part1, part2 = torch.split(noisy_latents, split_size, dim=dim)
         print(f"test_noisy_latent:part2:{part2.shape}")
-        #conv_output1 = self.conv1(part1.to(device))
-        #conv_output2 = self.conv1(part2.to(device))
-        #print(f"test_noisy_latent:conv_output2:{conv_output2.shape}")
         l2_pooled_output1 = self.lp_pool(part1)
         l2_pooled_output2 = self.lp_pool(part2)
         print(f"test_noisy_latent:l2_pooled_output2:{l2_pooled_output2.shape}")
