86fbd8c37049162ef89d1e894c8810932ee83ce1
bmaltais <bernard@ducourier.com>
2025-01-17 08:38:10 -0500

    Update gui-uv.bat and fix typos

--- gui-uv.bat
+++ gui-uv.bat
@@ -1,22 +1,20 @@
 @echo off
 
-set PYTHON_VER=3.10.9
-
-:: Update pip to latest version
+:: Install uv latest version
 pip install --upgrade uv -q
 
 set PATH=%PATH%;%~dp0venv\Lib\site-packages\torch\lib
 
-echo Starting the GUI... this might take some time...
+echo Starting the GUI... this might take some time... Especially on 1st run after install or update...
 
 :: If the exit code is 0, run the kohya_gui.py script with the command-line arguments
 if %errorlevel% equ 0 (
     REM Check if the batch was started via double-click
     IF /i "%comspec% /c %~0 " equ "%cmdcmdline:"=%" (
         REM echo This script was started by double clicking.
-        cmd /k uv run kohya_gui.py --noverify %*
+        cmd /k uv run --link-mode=copy kohya_gui.py --noverify %*
     ) ELSE (
         REM echo This script was started from a command prompt.
-        uv run kohya_gui.py --noverify %*
+        uv run --link-mode=copy kohya_gui.py --noverify %*
     )
 )
