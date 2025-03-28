b6a438592dae9a67da866bf84124affb589ac51c
bmaltais <bernard@ducourier.com>
2025-03-28 09:31:03 -0400

    Fix issue with bat script

--- gui-uv.bat
+++ gui-uv.bat
@@ -1,47 +1,23 @@
-#!/usr/bin/env bash
+@echo off
 
-# Checks to see if variable is set and non-empty.
-# This is defined first, so we can use the function for some default variable values
-env_var_exists() {
-  if [[ -n "${!1}" ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
+:: Install uv latest version
+pip install --upgrade uv -q
 
-# Define the directory path for WSL2
-lib_path="/usr/lib/wsl/lib/"
+set PATH=%PATH%;%~dp0venv\Lib\site-packages\torch\lib
 
-# Check if the directory exists
-if [ -d "$lib_path" ]; then
-    # Check if LD_LIBRARY_PATH is already set
-    if [ -z "${LD_LIBRARY_PATH}" ]; then
-        # LD_LIBRARY_PATH is not set, set it to the lib_path
-        export LD_LIBRARY_PATH="$lib_path"
-        # echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
-    fi
-fi
+echo Starting the GUI... this might take some time... Especially on 1st run after install or update...
 
-# If it is run with the sudo command, get the complete LD_LIBRARY_PATH environment variable of the system and assign it to the current environment,
-# because it will be used later.
-if [ -n "$SUDO_USER" ] || [ -n "$SUDO_COMMAND" ]; then
-    echo "The sudo command resets the non-essential environment variables, we keep the LD_LIBRARY_PATH variable."
-    export LD_LIBRARY_PATH=$(sudo -i printenv LD_LIBRARY_PATH)
-fi
-
-# This gets the directory the script is run from so pathing can work relative to the script where needed.
-SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
-
-# Step into GUI local directory
-cd "$SCRIPT_DIR" || exit 1
-
-# Check if uv is already installed
-if [ ! -f "$HOME/.local/bin/uv" ]; then
-  # Setup uv
-  curl -LsSf https://astral.sh/uv/install.sh | sh
-  source $HOME/.local/bin/env
-fi
-
+:: Make sure we are on the right sd-scripts commit
 git submodule update --init --recursive
-uv run kohya_gui.py --noverify "$@"
+
+:: If the exit code is 0, run the kohya_gui.py script with the command-line arguments
+if %errorlevel% equ 0 (
+    REM Check if the batch was started via double-click
+    IF /i "%comspec% /c %~0 " equ "%cmdcmdline:"=%" (
+        REM echo This script was started by double clicking.
+        cmd /k uv run --link-mode=copy --index-strategy unsafe-best-match kohya_gui.py --noverify %*
+    ) ELSE (
+        REM echo This script was started from a command prompt.
+        uv run --link-mode=copy --index-strategy unsafe-best-match kohya_gui.py --noverify %*
+    )
+)
